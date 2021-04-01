cf3bda36241bb759743b9c7a46af1d115ebc528f
Umamaheswararao Meka <maheshmeka16@gmail.com>
2021-04-02 02:46:10 +0530

    added int data types

---
+++ 1.main.py
@@ -0,0 +1,2 @@
+name = input('What is your name ? ')
+print('And the Name is : ' + name)
---
+++ 2.data-types.py
@@ -0,0 +1,10 @@
+
+print(2 + 4)
+print(2 - 4)
+print(2 * 4)
+print(2 / 4)
+print(type(2+4))
+print(type(2-4))
+print(type(2*4))
+print(type(2/4))
+
